17581080035b6fe7e753ad2144c3bdf31ad279ed
janus9266 <iamjanus66@gmail.com>
2024-09-01 11:53:24 -0400

    adding for google oauth

--- frontend/src/components/Layouts/DefaultLayout.tsx
+++ frontend/src/components/Layouts/DefaultLayout.tsx
@@ -1,22 +1,29 @@
-import React from "react";
+'use client'
+import React, { ReactNode } from "react";
 import Header from "@/components/Header";
 import Footer from "../Footer";
+import { SessionProvider } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
-const DefaultLayout = (props: {
-  children: React.ReactNode
-}) => {
+interface Props {
+  children: ReactNode;
+}
+
+const DefaultLayout = (props: Props) => {
   return (
-    <div className="flex h-screen overflow-hidden text-white">
-      <div className="relative flex flex-1 flex-col">
-        <Header />
-        <main>
-          <div className="mx-auto max-w-screen px-10 py-5">
-            {props.children}
-          </div>
-        </main>
-        <Footer />
+    <SessionProvider>
+      <div className="flex h-screen overflow-hidden text-white">
+        <div className="relative flex flex-1 flex-col">
+          <Header />
+          <main>
+            <div className="mx-auto max-w-screen px-10 py-5">
+              {props.children}
+            </div>
+          </main>
+          <Footer />
+        </div>
       </div>
-    </div>
+    </SessionProvider>
   );
 };
 
