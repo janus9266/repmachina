f9ee4aebfdf38eec9bb4c4ee0899d18bc18765ef
janus9266 <iamjanus66@gmail.com>
2024-09-23 10:43:58 -0400

    delete original projects

--- frontend/src/app/SessionProvider.js
+++
@@ -1,7 +0,0 @@
-"use client"; // This ensures it's a Client Component
-
-import { SessionProvider as NextAuthSessionProvider } from "next-auth/react";
-
-export default function SessionProvider({ children }) {
-  return <NextAuthSessionProvider>{children}</NextAuthSessionProvider>;
-}
--- frontend/src/types/user.ts
+++
@@ -1,6 +0,0 @@
-export type User = {
-  id: string,
-  avatar?: string,
-  name?: string
-  email: string,
-}
